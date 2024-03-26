bbbed94a2f9bd0abda23c57d87a0e398018dca52
DavidAyad97 <davidayad88@gmail.com>
2024-03-27 00:35:48 +0200

    first part of navbar done

--- Common/common.js
+++ Common/common.js
@@ -1,12 +1,65 @@
 //header and footer
 class specialHeader extends HTMLElement{
     connectedCallback(){
-        this.innerHTML=`<div id="header-container">     
-        <h3 class="text-center">header</h3>
-        <a href="./index.html"> <i class="fa-solid fa-house"></i> Home</a>
-        <a href="./about.html">About</a>
-    <a href="./categories.html">categories</a>
-    </div>
+        this.innerHTML=`    <header>
+        <div class="container-lg container-fluid">
+            <div  id="nav-header">
+
+                <div class="row">
+                   
+                    <!--header icons-->
+
+                    <div class="col-md-3 col-lg-4">
+                        <div class="header-icons text-white d-flex justify-content-center align-items-center h-100 py-1 py-md-0">
+
+                            <a class="icon pe-2" href="#">
+                                <i class="fa-solid fa-cart-shopping"></i>
+                                <span id="cart-counter">0</span>
+                            </a>
+                            <a class="icon pe-2" href="#">
+                                <i class="fa-solid fa-arrow-right-from-bracket "></i>
+                                logout
+                            </a>
+                            <a class="icon pe-2" href="#">
+                                <i class="fa-solid fa-arrow-right-to-bracket"></i>
+                                login
+                            </a>
+                        </div>
+                    </div>
+
+                    <!-- header logo -->
+
+                    <div class="col-md-4 col-lg-4">
+                        <div class="logo-container w-100 d-flex justify-content-center align-items-center py-1 py-md-0">
+                            <img src="./images/logo.png" alt=" logo">
+                        </div>
+                    </div>
+
+                    <!-- header search -->
+                    <div class="col-md-5 col-lg-4">
+                        <div class="d-flex justify-content-center align-items-md-center  h-100 flex-column flex-md-row py-1 py-md-0">
+
+                            <div id="phone-number" class=" text-center">
+                                <a href="tel:+123-456-789" class="pe-md-2 pe-1">123-456-789</a>
+                            </div>
+                            <div class="position-relative search">
+                                <input type="text" class="search-input w-100">
+                                <i class="fa-solid fa-magnifying-glass"></i>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            <div id="nav-bar">
+            <a href="./index.html"><i class="fa-solid fa-house"></i> Home</a>
+                <a href="./about.html">about</a>
+                <a href="./categories.html">categories</a>
+            </div>
+        </div>
+    </header>
     `;
     }
 }
